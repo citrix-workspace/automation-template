c56f9b2c222326b62b21dd6f0106fb23bcf65d53
dominikso <dominik.solc@citrix.com>
2021-08-23 15:14:28 +0200

    New way of exporting GitHub secrets with env variable

--- config.ts
+++ config.ts
@@ -1,21 +1,22 @@
+const secrets = JSON.parse(`${process.env.CONFIG_SECRETS}`);
+
 export const config = {
+    envSecrets: JSON.parse(`${process.env.CONFIG_SECRETS}`),
     headless: true,
     screenshotOnEachStep: false,
     stepScreenshots: true,
     devtools: false,
-    workspaceIdentityProvider: process.env.CONFIG_IDP ? `${process.env.CONFIG_IDP}` : 'ad',
-    workspaceUrl: process.env.CONFIG_WORKSPACE_URL
-        ? `${process.env.CONFIG_WORKSPACE_URL}`
-        : '',
-    workspaceUsername: process.env.CONFIG_WORKSPACE_USERNAME ? `${process.env.CONFIG_WORKSPACE_USERNAME}` : '',
-    workspacePassword: process.env.CONFIG_WORKSPACE_PASSWORD ? `${process.env.CONFIG_WORKSPACE_PASSWORD}` : '',
-    cwaAPI: process.env.CONFIG_CWA_API ? `${process.env.CONFIG_CWA_API}` : '',
-    citrixCloudCustomerId: process.env.CONFIG_CUSTOMER_ID ? `${process.env.CONFIG_CUSTOMER_ID}` : '',
-    citrixCloudClientId: process.env.CONFIG_CLIENT_ID ? `${process.env.CONFIG_CLIENT_ID}` : '',
-    citrixCloudClientSecret: process.env.CONFIG_CLIENT_SECRET ? `${process.env.CONFIG_CLIENT_SECRET}` : '',
-    microappsAdminUrl: process.env.CONFIG_MICROAPPS_ADMIN_URL ? `${process.env.CONFIG_MICROAPPS_ADMIN_URL}` : '',
-    loggerFilter: `.${process.env.CONFIG_BUILDER_URL}(.*)initiator_type=BUTTON`,
+    workspaceIdentityProvider: secrets.CONFIG_IDP ? `${secrets.CONFIG_IDP}` : 'ad',
+    workspaceUrl: secrets.CONFIG_WORKSPACE_URL ? `${secrets.CONFIG_WORKSPACE_URL}` : '',
+    workspaceUsername: secrets.CONFIG_WORKSPACE_USERNAME ? `${secrets.CONFIG_WORKSPACE_USERNAME}` : '',
+    workspacePassword: secrets.CONFIG_WORKSPACE_PASSWORD ? `${secrets.CONFIG_WORKSPACE_PASSWORD}` : '',
+    cwaAPI: secrets.CONFIG_CWA_API ? `${secrets.CONFIG_CWA_API}` : '',
+    citrixCloudCustomerId: secrets.CONFIG_CUSTOMER_ID ? `${secrets.CONFIG_CUSTOMER_ID}` : '',
+    citrixCloudClientId: secrets.CONFIG_CLIENT_ID ? `${secrets.CONFIG_CLIENT_ID}` : '',
+    citrixCloudClientSecret: secrets.CONFIG_CLIENT_SECRET ? `${secrets.CONFIG_CLIENT_SECRET}` : '',
+    microappsAdminUrl: secrets.CONFIG_MICROAPPS_ADMIN_URL ? `${secrets.CONFIG_MICROAPPS_ADMIN_URL}` : '',
+    loggerFilter: `.${secrets.CONFIG_BUILDER_URL}(.*)initiator_type=BUTTON`,
     defaultTimeout: 90000,
     shouldConfigureIntegrations: false,
-    loggErrors: true
+    loggErrors: true,
 };
